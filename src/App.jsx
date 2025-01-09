0ab1bd550f3c9cd5f3c655083c5ad8c821383e60
TemzA <kikotik91@gmail.com>
2025-01-09 23:27:31 +0300

    IO UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import './App.css'
 
 function App() {
   return (
-    <div>zxc</div>
+    <div className="app">
+      <h1>AI Application</h1>
+        <div className="flex-row">
+            <input placeholder="Enter prompt..."/>
+            <button>Send</button>
+        </div>
+        <textarea placeholder="Result..." />
+    </div>
   )
 }
 
